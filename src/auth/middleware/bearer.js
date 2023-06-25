f2fd6ba1a0659d75d67bbdfa9b4ce5f0dc92fa18
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-06-25 13:50:34 +0000

    adding else to if

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -7,12 +7,12 @@ module.exports = async (req, res, next) => {
   try {
 
     if (!req.headers.authorization) { _authError() }
-
+     else{
     const token = req.headers.authorization.split(' ').pop();
     const validUser = await usersModel.authenticateToken(token);
     req.user = validUser;
     req.token = validUser.token;
-    next();
+    next();}
 
   } catch (e) {
     console.log(e)
