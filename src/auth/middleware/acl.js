0610cdcc6389c03bb0e8f648bd405ea384a32d05
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-06-25 14:46:18 +0000

    get all try back

--- src/auth/middleware/acl.js
+++ src/auth/middleware/acl.js
@@ -4,14 +4,17 @@ module.exports = (capability) => {
 
   return (req, res, next) => {
 
-  
+    try {
       if (req.user.capabilities.includes(capability)) {
         next();
       }
       else {
         next('Access Denied');
       }
- 
+    } catch (e) {
+      next('Invalid Login');
+    }
+
   }
 
 }
--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -47,12 +47,14 @@ const userModel = (sequelize, DataTypes) => {
   };
 
   model.authenticateToken = async function (token) {
-   
+    try {
       const parsedToken = jwt.verify(token, SECRET);
       const user =await this.findOne({where: { username: parsedToken.username } });
       if (user) { return user; }
       throw new Error("User Not Found");
-    
+    } catch (e) {
+      throw new Error(e.message)
+    }
   };
 
   return model;
